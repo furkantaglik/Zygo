ffb97fb1e04d26381958e7112db5710b759940b9
furkan <furkantaglik2334@gmail.com>
2024-12-19 16:44:07 +0300

    vercel deploy configuratio n

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,10 @@
 import app from "./app.js";
-import startMongo from "./lib/db.js";
 import { serve } from "@hono/node-server";
 import initializeSocket from "./socket.js";
+import startMongo from "./lib/db.js";
 
-const port = 5000;
+const server = serve({ fetch: app.fetch });
+initializeSocket(server);
+await startMongo();
 
-async function startServer() {
-  try {
-    await startMongo();
-    console.log(`MongoDB bağlantısı başarılı.`);
-
-    const server = serve({ fetch: app.fetch, port: port });
-    initializeSocket(server);
-
-    console.log(`Server is running on http://localhost:${port}`);
-  } catch (error) {
-    console.error("Sunucu başlatılırken hata oluştu:", error);
-  }
-}
-
-startServer();
+console.log(`Server is running http://localhost:3000`);
